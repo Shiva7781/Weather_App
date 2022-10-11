9103536da12a32e59172630288f561700a567347
Shiva7781 <shivavishwakarma7781@gmail.com>
2022-10-11 18:48:35 +0530

    sun/moon for day/night

--- src/components/SunMovement.js
+++ src/components/SunMovement.js
@@ -31,87 +31,112 @@ const SunMovement = () => {
   //   console.log("currentData:", currentData);
 
   const { sunrise, sunset } = currentData;
-  //   console.log("sunrise", sunrise);
-  //   console.log("sunset", sunset);
+  // console.log("sunrise", sunrise);
+  // console.log("sunset", sunset);
 
-  const currentTime = new Date().getTime();
-  //   console.log("currentTime:", currentTime);
+  const currentSec = new Date().getTime();
+  // console.log("currentSec:", currentSec);
 
-  // Converting the seconds into time
-  let Drise = new Date(sunrise * 1000);
-  let SRise = `${Drise.getHours()}${Drise.getMinutes()}`;
+  /** */
+  /** Converting the seconds into time */
+
+  let riseTime = new Date(sunrise * 1000);
+  // console.log("riseTime:", riseTime);
+  let SRise = `${riseTime.getHours()}`;
   //   console.log("SRise:", SRise);
 
-  let Dset = new Date(sunset * 1000);
-  let SSet = `${Dset.getHours()}${Dset.getMinutes()}`;
+  let setTime = new Date(sunset * 1000);
+  let SSet = `${setTime.getHours()}`;
   //   console.log("SSet:", SSet);
 
-  let CurrentD = new Date(currentTime);
-  let CSun = `${CurrentD.getHours()}${CurrentD.getMinutes()}`;
-  //   console.log("CSun:", CSun);
+  let currentTime = new Date(currentSec);
+  let SCurrent = `${currentTime.getHours()}`;
+  //   console.log("SCurrent:", SCurrent);
 
-  let displaySun = [SRise, CSun, SSet];
-  //   let displaySun = [sunrise, currentTime, sunset];
-  console.log("displaySun:", displaySun);
+  // SSet = 7;
 
-  const pointImage = new Image(51, 51);
-  pointImage.src = "https://cdn-icons-png.flaticon.com/512/869/869869.png";
+  // eslint-disable-next-line
+  let displaySun = [SRise, SCurrent, SSet];
+  // console.log("displaySun:", displaySun);
+
+  const pointImage1 = new Image(77, 77);
+  pointImage1.src = "https://cdn-icons-png.flaticon.com/512/869/869869.png";
+
+  const pointImage2 = new Image(55, 55);
+  pointImage2.src = "https://cdn-icons-png.flaticon.com/128/1823/1823324.png";
+  // pointImage2.src = "https://cdn-icons-png.flaticon.com/128/2024/2024058.png";
+  // pointImage2.src = "https://cdn-icons-png.flaticon.com/128/547/547433.png";
 
   const [data, setData] = useState({
-    labels: ["Sunrise", "Current", "Sunset"],
+    labels: ["Sunrise", "", "Sunset"],
 
     datasets: [
       {
-        label: " Sun Movement",
-        data: [7, 18, 7],
-
+        label: " Sun Position",
+        data: [0, SCurrent, 0],
         backgroundColor: "white",
         borderWidth: "1",
         tension: "0.4",
-        //   borderColor: "rgb(28,169,244)",
 
-        //   pointStyle: "triangle",
-        pointStyle: ["triangle", pointImage, "triangle"],
+        // borderColor: "rgb(28,169,244)",
+        // pointStyle: "triangle",
 
-        //   pointHitRadius:"1",
+        pointStyle: ["triangle", pointImage1, "triangle"],
+        pointHitRadius: "1",
         pointRadius: 3,
         pointBorderWidth: 3,
         fill: {
           target: "origin",
-          above: " rgb(241, 241, 77, 0.3)",
-          below: "gray",
+          above: "rgb(241, 241, 77, 0.3)",
+          below: "rgb(53, 53, 53, 0.9)",
         },
       },
     ],
   });
 
-  useEffect(() => {
-    setData({
-      labels: ["Sunrise", "Current", "Sunset"],
+  /** */
+  /** Checking Day/Night */
 
+  useEffect(() => {
+    let A = "Sunrise";
+    let B = "Sunset";
+    let myLabel = " Sun Position";
+    let myFillColor = "rgb(241, 241, 77, 0.5)";
+    let myImage = pointImage1;
+
+    if (SCurrent > SSet) {
+      A = "Sunset";
+      B = "Sunrise";
+      myLabel = " Moon Position";
+      myFillColor = "rgb(53, 53, 53, 3)";
+      myImage = pointImage2;
+    }
+    console.log("SSet:", SSet);
+    console.log("SCurrent:", SCurrent);
+
+    setData({
+      labels: [A, "", B],
       datasets: [
         {
-          label: " Sun Movement",
-          data: [11, 55, 11],
-
-          backgroundColor: "white",
+          label: myLabel,
+          data: [0, SCurrent, 0],
+          backgroundColor: myFillColor,
           borderWidth: "1",
           tension: "0.4",
-          //   pointStyle: "triangle",
-          pointStyle: ["triangle", pointImage, "triangle"],
 
-          //   pointHitRadius:"1",
+          // borderColor: "rgb(28,169,244)",
+
+          pointStyle: ["triangle", myImage, "triangle"],
+          pointHitRadius: "1",
           pointRadius: 3,
           pointBorderWidth: 3,
-          fill: {
-            target: "origin",
-            above: " rgb(241, 241, 77, 0.3)",
-            below: "gray",
-          },
+          fill: true,
         },
       ],
     });
-  }, [displaySun]);
+
+    // eslint-disable-next-line
+  }, [SSet, SCurrent]);
 
   const options = {
     plugins: {
@@ -119,11 +144,15 @@ const SunMovement = () => {
         display: false,
       },
     },
+
     scales: {
-      y: {
+      x: {
         beginnZero: true,
+        // max: 25,
+
         ticks: {
-          display: false,
+          color: "teal",
+          font: { size: 15 },
         },
         grid: {
           display: false,
@@ -132,16 +161,18 @@ const SunMovement = () => {
         },
       },
 
-      x: {
+      y: {
         beginnZero: true,
+        max: 25,
+
         ticks: {
-          color: "teal",
-          font: { size: 15 },
+          display: false,
         },
+
         grid: {
           display: false,
           drawTicks: false,
-          //   drawBorder: false,
+          drawBorder: false,
         },
       },
     },
